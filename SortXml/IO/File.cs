17ddd6fc7c7f3ece4895ddd333dbe0231b6ffad4
gbsandeep <gbsandeep@gmail.com>
2014-07-14 15:02:43 -0700

    External library and new files

---
+++ SortXml/IO/File.cs
@@ -0,0 +1,42 @@
+﻿using SortXml.Error;
+using SortXml.Log;
+using System;
+using System.IO;
+using System.Text;
+
+namespace SortXml.IO {
+    internal static class File {
+        internal static string LoadInputFile(string inputFile, ILog log) {
+            string inputXml = string.Empty;
+            try {
+                using (var reader = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+                using (var streamReader = new StreamReader(reader, Encoding.Default)) {
+                    inputXml = streamReader.ReadToEnd();
+                }
+            }
+            catch (InputError ie) {
+                log.Error(ie);
+            }
+            catch (Exception e) {
+                log.Error(e);
+            }
+            return inputXml;
+        }
+
+        internal static void WriteOutputFile(string outputFile, string sortedData, ILog log) {
+            try {
+                using (var writer = new FileStream(outputFile, FileMode.OpenOrCreate, FileAccess.Write))
+                using (var streamWriter = new StreamWriter(writer, Encoding.Default)) {
+                    streamWriter.Write(sortedData);
+                }
+            }
+            catch (InputError ie) {
+                log.Error(ie);
+            }
+            catch (Exception e) {
+                log.Error(e);
+            }
+
+        }
+    }
+}
---
+++ SortXml/Settings/Settings.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using SortXml.Log;
+
+namespace SortXml.Settings {
+    internal class Settings {
+        [Option('i', "inputfile", Required = true, HelpText="Input XML file to be processed. File to contain syntactically valid XML data only.")]
+        public string InputFileName { get; set; }
+        [Option('d', "dryrun", Required = false, HelpText = "Dry run without generating the output file.")]
+        public bool DryRun { get; set; }
+        [Option('o', "outputfile", Required = true, HelpText = "Output file with sorted XML data.")]
+        public string OutputFileName { get; set; }
+        [Option('d', "depth", Required = false, HelpText = "Positive integer value to indicate the depth of the XML tree to be processed. Default is to parse the entire tree.")]
+        public uint Depth { get; set; }
+        [Option('l', "log", Required = false, HelpText = "Log level. Error = 0, Warning = 1, Info = 2, Log = 3. Default is to log error and warnings.")]
+        public Level LogLevel { get; set; }
+
+        [HelpOption]
+        public string GetUsage() {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+
+}
